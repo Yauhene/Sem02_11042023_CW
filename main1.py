69abab457c5d17410ae63342f390cacb69038f6e
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-04-11 22:08:37 +0300

    draft edition

---
+++ main1.py
@@ -0,0 +1,26 @@
+﻿for i in range(0, 5, 2):
+    print(i)
+
+stroka = "qwerty"
+for el in stroka:
+    print(el)
+
+
+i = 0
+while (i < 5):
+    n = int(input("Ввод: "))
+    if n == 0:
+        break
+    i+=1
+else:
+    print("Конец")
+#
+stroka = "qwerty"
+for el in stroka:
+    print(el, end=" ")
+print("qqqq")
+print("qqqq")
+#
+num = input("Введите ваше число: ")
+
+print(int(num[0]) + int(num[1]) + int(num[2]))
